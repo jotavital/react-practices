55990ff620a2930337af1d64afac88c870dd40bc
Joao Vital <joaopedrovitaldoctum@gmail.com>
2024-06-05 22:11:09 -0300

    ⚛️melhoria na home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link';
+
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<h1>Olá</h1>
+		<main className='flex min-h-screen flex-col items-center p-24'>
+			<h1 className={'text-4xl font-bold text-green-300 mb-5'}>
+				Clica em um link aí 👇
+			</h1>
+
+			<Link href={'suspense-api'} className={'text-blue-500'}>
+				Suspense API
+			</Link>
 		</main>
 	);
 }
